a220f719b4e007a1d1b5d707342ad08c57a3b92d
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-06 14:20:44 +0000

    0x01. ES6 Promises Mandatory Task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'success',
-    };
-  }).catch(() => new Error());
+  const myObject = {
+    status: 200,
+    body: 'success',
+  };
+
+  promise.then(() => myObject).then(() => console.log('Got a response from the API')).catch(() => new Error());
 }
