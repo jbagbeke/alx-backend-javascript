7e634407ac6d7e21f07340ff6d69a35d9d9776ea
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-06 17:52:30 +0000

    0x01. ES6 Promises Mandatory Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((returnPromises) => {
-    const returnArray = [];
-
-    for (const obj of returnPromises) {
-      if (obj) {
-        returnArray.append({
-          status: obj.status,
-          value: obj,
-        });
-      }
-    }
-  });
+  ]).then((returnPromises) => returnPromises);
 }
--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,8 +1,11 @@
 import handleProfileSignup from './6-final-user.js';
 
-async function testFunc() {
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+
+async function waiting() {
   const test = await handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
-  console.log(test);
 }
 
-testFunc();
+console.log();
+waiting();
+
