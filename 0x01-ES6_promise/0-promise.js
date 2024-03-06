1e6ca2274792049ae7cffc00e66fe141f8a41776
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-06 13:43:01 +0000

    0x01. ES6 Promises Mandatory Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,13 @@
 export default function getResponseFromAPI() {
-  return new Promise();
+  const newProomiseObject = new Promise((resolve, reject) => {
+    const i = 5;
+
+    if (i > 2) {
+      resolve('I is 2');
+    } else {
+      reject(RangeError);
+    }
+  });
+
+  return newProomiseObject;
 }
