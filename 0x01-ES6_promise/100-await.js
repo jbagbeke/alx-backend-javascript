a8c021da8a1ef7cf501affca863a99207d2a60da
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-06 22:54:51 +0000

    0x00. ES6 Basics Advanced Task 10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils'; 
+
+export default async function asyncUploadUser() {
+  return {
+    photo: await uploadPhoto().catch(() => null),
+    user: await createUser().catch(() => null),
+  };
+}
