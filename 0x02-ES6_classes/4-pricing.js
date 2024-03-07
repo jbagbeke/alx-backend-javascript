fd1c514719415c9b0f622b6180c758cee06a4db9
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-07 23:41:22 +0000

    0x02. ES6 classes Mandatory Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._verifyAmount(amount);
+    this._verifyCurrency(currency);
+  }
+
+  set currency(currency) {
+    this._verifyCurrency(currency);
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._verifyAmount(amount);
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  _verifyCurrency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Invalid Currency Object Passed');
+    }
+  }
+
+  _verifyAmount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Invalid amount value');
+    }
+  }
+
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.displayFullCurrency()}`);
+  }
+}
