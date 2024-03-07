22277d5a812e4f74545fcb2074c94149c9885581
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-07 17:29:22 +0000

    0x02. ES6 classes Mandatory Task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,16 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._verifyString(name);
+    this._verifyNumber(length);
+    this._verifyArray(students);
   }
 
   set name(name) {
-    if (typeof name === 'string') {
-      this._name = name; /* eslint no-underscore-dangle: 0 */
-    } else {
-      throw new TypeError('Name must be a string');
-    }
+    this._verifyString(name);
   }
 
   get name() {
@@ -18,11 +14,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
+    this._verifyNumber(length);
   }
 
   get length() {
@@ -30,12 +22,34 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (Array.isArray(students) && (students.every((element) => typeof element === 'string' && element.trim() !== ''))) {
-      this._students = students;
-    }
+    this._verifyArray(students);
   }
 
   get students() {
     return this._students;
+  }
+
+  _verifyArray(array) {
+    if (Array.isArray(array) && (array.every((element) => typeof element === 'string' && element.trim() !== ''))) {
+      this._students = array;
+    } else {
+      throw new TypeError('Students must be an array');
+    }
+  }
+
+  _verifyString(string) {
+    if (typeof string === 'string') {
+      this._name = string; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  _verifyNumber(number) {
+    if (typeof number === 'number') {
+      this._length = number;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 }
