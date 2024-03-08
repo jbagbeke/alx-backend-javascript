b44cdc3607d33644d101650eef2df90d820fafcc
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-08 02:19:17 +0000

    0x02. ES6 classes Mandatory Task 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,31 +1,91 @@
 export class HolbertonClass {
   constructor(year, location) {
-    this._year = year;
-    this._location = location;
+    this._verifyNumber(year);
+    this._verifyString(location);
   }
 
   get year() {
     return this._year;
   }
 
+  set year(year) {
+    this._verifyNumber(year);
+  }
+
   get location() {
     return this._location;
+  }
+
+  set location(location) {
+    this._verifyString(location);
+  }
+
+  _verifyString(string) {
+    if (typeof string === 'string') {
+      this._name = string; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  _verifyNumber(number) {
+    if (typeof number === 'number') {
+      this._length = number;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 }
 
 export class StudentHolberton {
   constructor(firstName, lastName, classObject) {
-    this._firstName = firstName; /* eslint no-underscore-dangle: 0 */
-    this._lastName = lastName;
-    this._holbertonClass = classObject;
+    this._verifyString(firstName, 'first'); /* eslint no-underscore-dangle: 0 */
+    this._verifyString(lastName, 'last');
+    this._verifyClass(classObject);
   }
 
   get fullName() {
     return `${this._firstName} ${this._lastName}`; /* eslint max-classes-per-file: 0 */
   }
 
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(firstName) {
+    this._verifyString(firstName, 'first');
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(lastName) {
+    this._verifyString(lastName, 'last');
+  }
+
   get holbertonClass() {
     return this.holbertonClass;
+  }
+
+  _verifyString(string, desc) {
+    if (typeof string === 'string') {
+      if (desc === 'first') {
+        this._firstName = string;
+      } else {
+        this._lastname = string; /* eslint no-underscore-dangle: 0 */
+      }
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  _verifyClass(currentClass) {
+    if (currentClass instanceof HolbertonClass) {
+      this._holbertonClass = currentClass; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Invalid Currency Object Passed');
+    }
   }
 
   get fullStudentDescription() {
