b834e1fa588f7ecd193a838b58b762a6b4e493be
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-08 00:06:16 +0000

    0x02. ES6 classes Mandatory Task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    this._verifySQFT(sqft);
+
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  set sqft(sqft) {
+    this._verifySQFT(sqft);
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  _verifySQFT(sqft) {
+    if (typeof sqft === 'number') {
+      this._sqft = sqft; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Invalid SQFT value');
+    }
+  }
+}
