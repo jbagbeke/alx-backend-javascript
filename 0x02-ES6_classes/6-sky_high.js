da83fb5faf1316c9b62ca48b172a1e95a064aa70
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-08 00:53:30 +0000

    0x02. ES6 classes Mandatory Task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from "./5-building.js";
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._verifyFloor(floors);
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._verifyFloor(floors);
+  }
+
+  _verifyFloor(floors) {
+    if (typeof floors === 'number') {
+      this._floors = floors; /* eslint no-underscore-dangle: 0 */
+    } else {
+      throw new TypeError('Invalid Floor Value Entered');
+    }
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
+}
