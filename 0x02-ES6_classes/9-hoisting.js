82221f13c8b056014e9659ac522333171f101f90
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-08 02:00:38 +0000

    0x02. ES6 classes Mandatory Task 9

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,45 @@
+export class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, classObject) {
+    this._firstName = firstName; /* eslint no-underscore-dangle: 0 */
+    this._lastName = lastName;
+    this._holbertonClass = classObject;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`; /* eslint max-classes-per-file: 0 */
+  }
+
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export const listOfStudents = [student1, student2, student3, student4, student5];
