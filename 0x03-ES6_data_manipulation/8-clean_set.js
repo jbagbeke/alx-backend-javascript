70262003e17a5b8d3d4c42db95b0e6faa70e1682
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-13 18:02:15 +0000

    0x03. ES6 data manipulation Mandatory Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
   const stringArray = [];
 
+  if (startString.length <= 0) {
+    return '';
+  }
+
   set.forEach((element) => {
     if (element.startsWith(startString)) {
       stringArray.push(element.slice(startString.length));
