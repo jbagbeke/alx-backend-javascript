08452265ac438569476e96bd99d208d5a671a251
Joshua Bubune <joshua.legithive@gmail.com>
2024-03-13 19:07:01 +0000

    0x03. ES6 data manipulation Mandatory Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,7 +6,7 @@ export default function cleanSet(set, startString) {
   const setArray = Array.from(set);
 
   const stringArray = setArray.filter((element) => (element !== undefined) || (element.length > 0) ? element.startsWith(startString) : '')
-    .map((element) => element.slice(startString.length)).join('-'); /* eslint no-confusing-arrow: 0 */
+    .map((element) => element !== undefined || element.length > 0 ? element.slice(startString.length) : '').join('-'); /* eslint no-confusing-arrow: 0 */
 
   return stringArray;
 }
