b60b1f929e031f362eb0b8f6b8aa2b696bcfb78d
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-30 13:48:19 +0000

    0x05. NodeJS Basics Mandatory Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.on('beforeExit', () => {
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data}`);
-})
+});
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
