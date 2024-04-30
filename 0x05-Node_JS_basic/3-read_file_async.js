c7ebe9bb348b6796f38aadd396118edf3b34c1e7
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-04-30 22:59:14 +0000

    0x05. NodeJS Basics Mandatory Task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      }
+      const studentFields = {};
+      let count = 0;
+      const lines = data.split('\n');
+      lines.forEach((line) => {
+        const lne = line.split(',');
+        const field = lne[lne.length - 1];
+        if (lne.length && field && field !== 'field') {
+          count += 1;
+          if (field in studentFields) {
+            studentFields[field].push(line[0]);
+          } else {
+            studentFields[field] = [lne[0]];
+          }
+        }
+      });
+
+      console.log(`Number of students: ${count}`);
+      for (const [key, val] of Object.entries(studentFields)) {
+        console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
