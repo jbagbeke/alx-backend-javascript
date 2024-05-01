28e2f00cbc7218ffb220756ea8ca4ec3bd293219
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-01 15:04:19 +0000

    0x06. Unittests in JS Mandatory Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,32 @@
-const calculateNumber = require('./0-calcul')
-const assert = require('assert')
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
 
 /* eslint no-undef: 0 */
 
 describe('Function', function () {
   describe('Addition Calculation', function () {
-    it('should return ceil of addition', function () {
-      assert.equal(calculateNumber(1, 3), 4)
+    it('return a + b', function () {
+      assert.equal(calculateNumber(1, 3), 4); 
+      assert.equal(calculateNumber(6, 2), 8); 
+      assert.equal(calculateNumber(0, 2), 2); 
     })
-    it('should return ceil of addition', function () {
-      assert.equal(calculateNumber(1, 3.7), 5)
+    it('a + round of b', function () {
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1, 3.2), 4);
+      assert.equal(calculateNumber(2, 3.9), 6);
+      assert.equal(calculateNumber(3, 3.1), 6);
     })
-    it('should return ceil of addition', function () {
-      assert.equal(calculateNumber(1.2, 3.7), 5)
+    it('round of a + round of b', function () {
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.5, 3.1), 5);
+      assert.equal(calculateNumber(1.4, 3.5), 5);
+      assert.equal(calculateNumber(1.9, 5.7), 8);
     })
-    it('should return ceil of addition', function () {
-      assert.equal(calculateNumber(1.5, 3.7), 6)
-    })
-    it('should return ceil of addition', function () {
-      assert.equal(calculateNumber(0, 0.1), 1)
+    it('a + round of b', function () {
+      assert.equal(calculateNumber(0, 0.1), 0);
+      assert.equal(calculateNumber(0, 3.7), 4);
+      assert.equal(calculateNumber(4, 4.1), 8);
+      assert.equal(calculateNumber(8, 1.1), 9);
     })
   })
 })
