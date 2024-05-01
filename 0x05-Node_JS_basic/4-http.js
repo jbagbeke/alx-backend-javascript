2e93968be2e2e32c7b527b7e71ccdc441986ac77
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-01 00:56:52 +0000

    0x05. NodeJS Basics Mandatory Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
-
 const app = http.createServer((request, response) => {
   response.end('Hello Holberton School!');
 });
 
 app.listen(1245);
+
+module.exports = app;
