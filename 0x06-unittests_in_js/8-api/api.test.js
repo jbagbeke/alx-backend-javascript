6a6f28956b47785d45daf19ed2f6e90cf522ee34
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-02 22:28:23 +0000

    0x06. Unittests in JS Mandatory Task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,29 +1,13 @@
 const { expect } = require('chai')
-// const axios = require('axios')
-const request = require('request');
+const axios = require('axios')
 
 /* eslint no-undef: 0 */
 describe('API TEST SUITE', () => {
-
-
     it('Test API response message', (done) => {
-        request('http://localhost:7865', (error, response, body) => {
-            if (error) {
-                console.error(`Error: ${error}`);
-                return;
-            }
-            
-            expect(body).to.equal('Welcome to the payment system')
-            expect(response.statusCode).to.equal(200);
-            done();
-        });
+        axios.get('http://127.0.0.1:7865').then((response) => {
+            expect(response.data).to.equal('Welcome to the payment system')
+            expect(response.status).to.equal(200);
+            done();       
+        })
     })
-
-    // it('Test API response message', (done) => {
-    //     axios.get('http://127.0.0.1:7865').then((response) => {
-    //         expect(response.data.slice(0, -1)).to.equal('Welcome to the payment system')
-    //         expect(response.status).to.equal(200);
-    //         done();       
-    //     })
-    // })
 })
