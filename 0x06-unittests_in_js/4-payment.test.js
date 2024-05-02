085ceda4ea0996908f4ef6230a6646a9c46e8730
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-02 14:57:54 +0000

    0x06. Unittests in JS Mandatory Task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,22 +4,13 @@ const Utils = require('./utils')
 const sendPaymentRequestToApi = require('./4-payment')
 
 const consoleSpy = sinon.spy(console, 'log')
+const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10)
 
 
 /* eslint no-undef: 0 */
 describe('Sinon Stub Test on Utils.calculateNumber', function() {
-    describe('CalculateNumber Test', function() {
-        let calculateNumberStub;
-
-        this.beforeEach(() => {
-            calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10)
-        })
-
-        this.afterEach(() => {
-            calculateNumberStub.restore()
-        })
-
-        it('stubs calculateNumber to return 10', function() {
+    describe('CalculateNumber Test', () => {
+        it('stubs calculateNumber to return 10', () => {
             sendPaymentRequestToApi(100, 20)
             expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true
             expect(consoleSpy.calledWith('The total is: 10')).to.be.true
