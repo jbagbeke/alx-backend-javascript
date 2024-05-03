33c0e338e06c6397cea8bf93fae88cf9359ff82d
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-03 01:05:26 +0000

    0x06. Unittests in JS Mandatory Task 10

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`)
+})
+
+app.get('/available_payments', (req, res) => {
+    res.send({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      })
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.userName
+    res.send(`Welcome ${username}`)
+})
+
+app.listen(7865, () => {
+    console.log('API available on localhost port 7865');
+})
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai')
+const axios = require('axios')
+
+/* eslint no-undef: 0 */
+describe('API TEST SUITE - ENDPOINT /', () => {
+    it('Test API response message', (done) => {
+        axios.get('http://127.0.0.1:7865').then((response) => {
+            expect(response.data).to.equal('Welcome to the payment system')
+            expect(response.status).to.equal(200);
+            done();       
+        })
+    })
+})
+
+describe('API TEST SUITE - ENDPOINT /login', () => {
+    it('Test API endpoint /login with valid data', (done) => {
+        axios.post('http://127.0.0.1:7865/login', { userName: 'Holberton' }).then((response) => {
+            expect(response.data).to.equal('Welcome Holberton')
+            expect(response.status).to.equal(200);
+            done();       
+        })
+    })
+    
+    it('Test API endpoint /login without data', (done) => {
+        axios.get('http://127.0.0.1:7865/login').catch((error) => {
+            expect(error.response.status).to.equal(404);
+            done();
+        })
+    })
+})
+
+describe('API TEST SUITE - ENDPOINT /available_payments', () => {
+    it('Test API endpoint response msg /available_payments', (done) => {
+        axios.get('http://127.0.0.1:7865/available_payments').then((response) => {
+            expect(response.data.payment_methods).to.deep.equal({ credit_cards: true, paypal: false })
+            expect(response.status).to.equal(200);
+            done();       
+        })
+    })
+    
+    it('Test API endpoint with typo /available_payments', (done) => {
+        axios.get('http://127.0.0.1:7865/available_paymentss').catch((error) => {
+            expect(error.response.status).to.equal(404);
+            done();
+        })
+    })
+})
