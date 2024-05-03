d61a96b506f73a9ca53d369b3664478fa6ff318e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-05-03 01:50:51 +0000

    0x06. Unittests in JS Mandatory Task 10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -12,6 +12,23 @@ describe('API TEST SUITE - ENDPOINT /', () => {
     })
 })
 
+describe('API TEST SUITE - ENDPOINT /cart/:id', () => {
+    it('Test API endpoint /cart/123', (done) => {
+        axios.get('http://127.0.0.1:7865/cart/123').then((response) => {
+            expect(response.data).to.equal('Payment methods for cart 123')
+            expect(response.status).to.equal(200);
+            done();       
+        })
+    })
+    
+    it('Test API endpoint /cart/abc', (done) => {
+        axios.get('http://127.0.0.1:7865/cart/abc').catch((error) => {
+            expect(error.response.status).to.equal(404);
+            done();
+        })
+    })
+})
+
 describe('API TEST SUITE - ENDPOINT /login', () => {
     it('Test API endpoint /login with valid data', (done) => {
         axios.post('http://127.0.0.1:7865/login', { userName: 'Holberton' }).then((response) => {
@@ -25,7 +42,7 @@ describe('API TEST SUITE - ENDPOINT /login', () => {
 describe('API TEST SUITE - ENDPOINT /available_payments', () => {
     it('Test API endpoint response msg /available_payments', (done) => {
         axios.get('http://127.0.0.1:7865/available_payments').then((response) => {
-            expect(response.data.payment_methods).to.deep.equal({ credit_cards: true, paypal: false })
+            expect(response.data).to.be.deep.equal({ payment_methods: { credit_cards: true, paypal: false }})
             expect(response.status).to.equal(200);
             done();       
         })
